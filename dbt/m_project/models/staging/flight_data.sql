5d6f7aa33664aa29c5e5907653e9bf03f44e470b
unknown <ukokobili.jacob@gmail.com>
2022-08-18 22:58:11 +0100

    dbt model update

--- dbt/m_project/models/staging/flight_data.sql
+++ dbt/m_project/models/staging/flight_data.sql
@@ -1,3 +1,23 @@
 {{ config(materialized='view') }}
 
-select * from {{ source('staging','flights') }}
+select
+
+    -- flight location
+    cast(Origin as string) as origin_id,			
+    cast(Destination as string) as destination_id,			
+    cast(Origin_City as string) as origin_city,			
+    Origin_State					
+    Destination_City					
+    Destination_State	
+
+    -- date
+    cast(Date as date) fligt_date,
+    				
+    Passengers	INTEGER				
+    Seats	INTEGER				
+    Flights	INTEGER				
+    Distance	FLOAT				
+    Origin_Population	INTEGER				
+    Destination_Population	INTEGER		
+
+from {{ source('staging','flights') }}
