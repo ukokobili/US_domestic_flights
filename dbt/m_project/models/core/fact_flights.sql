75b4e06668781adb9bc49a6cedea7c9f7e0d6dd0
unknown <ukokobili.jacob@gmail.com>
2022-08-22 20:55:07 +0100

    update model

--- dbt/m_project/models/core/fact_flights.sql
+++ dbt/m_project/models/core/fact_flights.sql
@@ -5,12 +5,8 @@ with
 a as (
     select *
     from {{ ref('flight_data') }}
-), 
-
-b as (
-    select * 
-    from {{ ref('dim_code') }}
 )
+
 select
     a.origin_code,				
     a.destination_code,					
@@ -24,13 +20,5 @@ select
     a.no_of_flights,				
     a.distance,				
     a.origin_population,				
-    a.destination_population,
-    x.airport_name as origin_airport,
-    x.lat_lon as origin_lat_lon,
-    y.airport_name as destination_airport,
-    y.lat_lon as destination_lat_lon
+    a.destination_population
 from a
-inner join b as x
-on a.origin_city = x.city
-inner join b as y
-on a.destination_city = y.city
--- dbt/m_project/models/staging/flight_data.sql
+++ dbt/m_project/models/staging/flight_data.sql
@@ -25,9 +25,3 @@ select
 
 from {{ source('staging','flights') }}
 
--- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
